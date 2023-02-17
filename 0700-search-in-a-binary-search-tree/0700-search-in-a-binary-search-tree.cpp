9adb93e28c15a778d22297ca4aba7f11a60b3d1e
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-02-17 22:37:18 +0530

    Time: 38 ms (88.44%), Space: 34.9 MB (35.55%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -14,8 +14,7 @@ public:
     TreeNode* searchBST(TreeNode* root, int val) {
      while(root!=NULL){
          if(root->val==val) {
-             TreeNode *head=root;
-             return head;
+             return root;
          }
          else if(root->val > val)root=root->left;
          else{
