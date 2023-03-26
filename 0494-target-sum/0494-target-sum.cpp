296d970d851e515941b04f8420a7bf86c0c440e5
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-03-26 16:12:52 +0530

    Time: 10 ms (86.64%), Space: 9.1 MB (84.93%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,41 +1,52 @@
 class Solution {
 public:
+     int issubset(vector<int>& nums,int sum,int n){
+         int dp[n+1][sum+1];
+         dp[0][0]=1;
+         for(int i=1;i<=sum;i++){
+             dp[0][i]=0;
+         }
+         
+         for(int i=1;i<=n;i++){
+             for(int j=0;j<=sum;j++){
+                 if(j<nums[i-1]){
+                     dp[i][j]=dp[i-1][j];
+                 }
+                 else{
+                     dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
+                 }
+             }
+         }
+         
+         
+         return dp[n][sum];
+         
+         
+         
+         
+     }
     
-     int  isSubsetSum(vector<int>arr, int sum){
-        //start your code
-    int n=arr.size();
-   int dp[n+1][sum+1];
-   dp[0][0]=1;
-    // for(int i=0;i<=n;i++){
-    //     dp[i][0]=1;
-    // }
-    for(int i=1;i<=sum;i++){
-        dp[0][i]=0;
-    }
-    // writ your code Here Patwa Ji
-    for(int i=1;i<=n;i++){
-        for(int j=0;j<=sum;j++){
-            if(j<arr[i-1]){
-                dp[i][j]=dp[i-1][j];
-            }
-            else{
-                dp[i][j]=dp[i-1][j]+dp[i-1][j-arr[i-1]];
-            }
-        }
-    }
-     return dp[n][sum];  
-    }
     
- 
+    
+    
+    
+    
+    
+    
+    
     int findTargetSumWays(vector<int>& nums, int target) {
-      int n=nums.size();
-        int sum=0;
-        for(int i:nums){
-            sum+=i;
+     int n=nums.size();int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
         }
-         if(sum < abs(target) || (target+sum) %2 != 0) return 0;  
-        int s1=(target+sum)/2;
-        return isSubsetSum(nums,s1);
+        // base condition
+        if(sum< abs(target) ||(sum+target)%2!=0) return 0;
+        
+        int s1=(target+ sum)/2;
+        return issubset(nums,s1,n);
+        
+        
+        
         
     }
 };
