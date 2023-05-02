603d7146072fc853efdc360813267d833175124c
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-05-02 19:50:05 +0530

    Time: 8 ms (27.40%), Space: 10.2 MB (45.94%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -3,9 +3,13 @@ public:
     int arraySign(vector<int>& nums) {
        int mul=1;;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]<0){mul*=-1;}
+            if(nums[i]<0){mul=-mul;}
             else if(nums[i]==0)return 0;
             }
+        
+        
+        
+        
             return mul;
     }
 };
