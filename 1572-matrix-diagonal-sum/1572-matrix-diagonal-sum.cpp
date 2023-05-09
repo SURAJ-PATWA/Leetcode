0bf9fc0e7e1dd80806413945a9cedc1890bfd540
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-05-10 02:16:32 +0530

    Time: 15 ms (56.99%), Space: 11.2 MB (91.86%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& arr) {
-       int n=arr.size();
-       int sum=0;
+       int N=arr.size();
+       
+        int sum1=0;
+       
+        for(int i=0;i<N;i++){
+            sum1+=arr[i][i];
+           
+        }
+        
+        
+        for(int i=0;i<N;i++){
+            
+            
+            if(i!=N-i-1){
+                sum1+=arr[i][N-i-1]; 
+            }
+        }
+        return sum1;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
        // first we will find first digonal sum 
-       for(int i=0;i<n;i++){
-           for(int j=0;j<n;j++){
-               if(i==j){
-                   sum+=arr[i][j];
-               }
-           }
-       }
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n;j++){
+//                if(i==j){
+//                    sum+=arr[i][j];
+//                }
+//            }
+//        }
 
-       for(int i=0;i<n;i++){
-           for(int j=n-i-1;j>=0;j--){
-               if(i==j){continue;}
-               if(i==n-j-1){sum+=arr[i][j];}
+//        for(int i=0;i<n;i++){
+//            for(int j=n-i-1;j>=0;j--){
+//                if(i==j){continue;}
+//                if(i==n-j-1){sum+=arr[i][j];}
               
-           }
-       }
-       return sum;
+//            }
+//        }
+//        return sum;
     }
 };
