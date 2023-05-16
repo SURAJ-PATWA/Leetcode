973db33b0ed0bf60230eee730e352422d42c0562
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-05-17 02:05:42 +0530

    Time: 6 ms (30.56%), Space: 7.5 MB (87.90%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -24,10 +24,3 @@ public:
         
     }
 };
-//  if(!head || !head->next) return head;
-//         ListNode* temp;
-//         temp = head->next;
-//         head->next = swapPairs(head->next->next);
-//         temp->next = head;
-        
-//         return temp;
