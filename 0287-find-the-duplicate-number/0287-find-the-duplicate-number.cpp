881732a1b11ed10f1d6a51228fc0ac91cc0cde32
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-06-19 19:04:05 +0530

    Time: 311 ms (7.84%), Space: 100.6 MB (8.83%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+     unordered_map<int,int>m;
+     for(int i=0;i<nums.size();i++){
+         m[nums[i]]++;
+     } 
+     for(auto i:m){
+         if(i.second>1){
+             return i.first;
+         }
+     }
+return -1;
+    }
+};
