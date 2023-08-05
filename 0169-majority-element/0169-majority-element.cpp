aee508f1e72ab906bb97c4a1da7f04604263c0c8
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-08-05 13:05:46 +0530

    Time: 16 ms (81.25%), Space: 19.6 MB (52.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,13 +1,41 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int>mp;int n=nums.size();
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        // Using Moore's Voting Algorithm
+        int count=0;int element;int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(count==0){
+                element=nums[i];
+                count++;
+            }
+            else if(nums[i]==element){
+                count++;
+            }
+            else{
+                count--;
+            }
         }
-        for(auto i:mp){
-            if(i.second>n/2)return i.first;
-        }
+        // now we will find this elemnt really grater than n/2
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==element)cnt++;
+            }
+        if(cnt>n/2)return element;
         return -1;
+        
+        
+        
+        
+        
+        
+        
+        // map<int,int>mp;int n=nums.size();
+        // for(int i=0;i<nums.size();i++){
+        //     mp[nums[i]]++;
+        // }
+        // for(auto i:mp){
+        //     if(i.second>n/2)return i.first;
+        // }
+        // return -1;
     }
 };
