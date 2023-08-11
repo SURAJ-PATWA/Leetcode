993e0147f5a8d0694d736a4d1f655e6b6d711444
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-08-11 20:12:39 +0530

    Time: 7 ms (95.48%), Space: 11.3 MB (13.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target) {int n=nums.size();
+        //Better brute force
         vector<int>ans;
         map<int,int>mp;
         for(int i=0;i<nums.size();i++){
@@ -11,5 +12,20 @@ public:
          mp[nums[i]]=i;
         }
         return{-1};
+        
+                                                    // // optimal Solution
+                                                    //       int s=0;int e=n-1;
+                                                    //    //sort(nums.begin(),nums.end());
+                                                    //    while(s<e){
+                                                    //        int sum=nums[s]+nums[e];
+                                                    //        if(sum==target)return {s,e};
+                                                    //        else if(sum<target){
+                                                    //            s++;
+                                                    //        }
+                                                    //        else{
+                                                    //            e--;
+                                                    //        }
+                                                    //    }
+                                                    //    return{-1};
     }
 };
