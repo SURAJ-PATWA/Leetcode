94b086d6a69978ed386f448404fe45e3ed1c9a3b
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-08-11 20:52:17 +0530

    Time: 13 ms (87.99%), Space: 19.6 MB (56.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,41 +1,47 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        // Using Moore's Voting Algorithm
-        int count=0;int element;int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(count==0){
-                element=nums[i];
-                count++;
+class Solution
+{
+    public:
+        int majorityElement(vector<int> &nums)
+        {            
+           // 2nd Method               
+           	// Using Moore's Voting Algorithm
+            int count = 0;
+            int element;
+            int n = nums.size();
+            for (int i = 0; i < n; i++)
+            {
+                if (count == 0)
+                {
+                    element = nums[i];
+                    count++;
+                }
+                else if (nums[i] == element)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
             }
-            else if(nums[i]==element){
-                count++;
+           	// now we will find this elemnt really grater than n/2
+            int cnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] == element) cnt++;
             }
-            else{
-                count--;
-            }
+            if (cnt > n / 2) return element;
+            return -1;
+            
+            //1st method
+
+           	// map<int,int>mp;int n=nums.size();
+           	// for(int i=0;i < nums.size();i++){
+           	//     mp[nums[i]]++;
+           	// }
+           	// for(auto i:mp){
+           	//     if(i.second>n/2)return i.first;
+           	// }
+           	// return -1;
         }
-        // now we will find this elemnt really grater than n/2
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==element)cnt++;
-            }
-        if(cnt>n/2)return element;
-        return -1;
-        
-        
-        
-        
-        
-        
-        
-        // map<int,int>mp;int n=nums.size();
-        // for(int i=0;i<nums.size();i++){
-        //     mp[nums[i]]++;
-        // }
-        // for(auto i:mp){
-        //     if(i.second>n/2)return i.first;
-        // }
-        // return -1;
-    }
 };
