956ef821b3c369af340ea7f6a0dc51f26a5e376f
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-08-12 10:16:03 +0530

    Time: 155 ms (92.49%), Space: 123.8 MB (64.98%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -2,21 +2,35 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& arr) {
         int n=arr.size();
-         vector<int>pos;  // 9 4 5 0  2
-	    vector<int>neg;  // -2 -1 -5 -3
-	    for(int i=0;i<n;i++){
-	    if(arr[i]>=0){
-	        pos.push_back(arr[i]);
-	    }
-	    else{
-	       neg.push_back(arr[i]);
-	    }
-	    }
-        for(int i=0;i<n/2;i++){
-            arr[2*i]=pos[i];
-            arr[2*i+1]=neg[i];
+        
+        //using striver method
+        vector<int>ans(n,0);int PosIndex=0,NegIndex=1;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]<0){
+                ans[NegIndex]=arr[i];NegIndex+=2;
+            }else{
+                ans[PosIndex]=arr[i];
+                PosIndex+=2;
+            }
+            
         }
-        return arr;
+        return ans;
+//          vector<int>pos;  // 9 4 5 0  2
+// 	    vector<int>neg;  // -2 -1 -5 -3
+// 	    for(int i=0;i<n;i++){
+// 	    if(arr[i]>=0){
+// 	        pos.push_back(arr[i]);
+// 	    }
+// 	    else{
+// 	       neg.push_back(arr[i]);
+// 	    }
+// 	    }
+//         for(int i=0;i<n/2;i++){
+//             arr[2*i]=pos[i];
+//             arr[2*i+1]=neg[i];
+//         }
+//         return arr;
 	    
 	   
 // 	    int i=0,j=0,k=0;
