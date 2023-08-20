2eeafe24fe2ceddc5f420978331bc1bfb99adb34
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-08-20 23:45:05 +0530

    Time: 8 ms (87.63%), Space: 8.3 MB (71.45%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     string frequencySort(string s) {
-       priority_queue<pair<int,char>>mp; //maxheap according to frequency of characters
+       priority_queue<pair<int,char>>maxheap; //maxheap according to frequency of characters
         unordered_map<char,int>freq;  //for frequency of characters
         for(char i:s){
             freq[i]++;
         }
         for(auto i:freq){
-            mp.push({i.second,i.first}); //heap will be constructed on the basis of frequency
+            maxheap.push({i.second,i.first}); //heap will be constructed on the basis of frequency
         }
         s="";
-        while(!mp.empty()){
-            s+=string(mp.top().first,mp.top().second);
-            mp.pop();
+        while(!maxheap.empty()){
+            s+=string(maxheap.top().first,maxheap.top().second);
+            maxheap.pop();
         }
         return s;
         
