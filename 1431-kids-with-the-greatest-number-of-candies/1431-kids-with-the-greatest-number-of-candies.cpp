4ffb68130f0c3bf76d367cfe2d0957588b73ddc9
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-08-29 19:48:07 +0530

    Time: 5 ms (25.84%), Space: 9 MB (33.96%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -15,10 +15,3 @@ public:
         return ans;
     }
 };
-// vector<bool> ans;
-        // int maxi = *max_element(candies.begin(), candies.end());
-        // for(auto it : candies)  {
-        //     if(it + extraCandies >= maxi)   ans.push_back(true);
-        //     else    ans.push_back(false);
-        // }
-        // return ans;
