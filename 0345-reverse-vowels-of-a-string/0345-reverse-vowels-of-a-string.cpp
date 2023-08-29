f5b1d953eb172ab85bc027347c179b7737ea229c
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-08-29 20:58:36 +0530

    Time: 3 ms (96.46%), Space: 7.7 MB (78.89%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -6,14 +6,7 @@ public:
     }
     string reverseVowels(string s) {
       int i=0,j=s.size()-1;
-      while(i<j){
-      //     if((s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') & (s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u') ){
-      //         swap(s[i],s[j]);
-      //     }
-      //     else{
-      //         i++;j--;
-      //     }
-      // }  
+      while(i<j){ 
           if(isvowel(s[i]) && isvowel(s[j])){
               swap(s[i],s[j]);
               i++;j--;
