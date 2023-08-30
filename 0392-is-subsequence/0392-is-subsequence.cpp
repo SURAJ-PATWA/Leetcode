1e342efed16037f57c81d85894ed9cbf4d3cdd73
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-08-30 17:42:41 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (92.37%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,42 +1,12 @@
 class Solution {
 public:
-     int lcs(int m, int n, string s1, string s2)
-    {
-        // your code here
-     
-        
-        int dp[m+1][n+1];
-       for(int i=0;i<=m;i++){
-           dp[i][0]=0;
-       }
-        for(int i=0;i<=n;i++){
-            dp[0][i]=0;
-        }
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-             if(s1[i-1]==s2[j-1]){
-          dp[i][j]=  1+dp[i-1][j-1];
-        }
-        else{
-            dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
-        }   
+    bool isSubsequence(string s, string t) {
+         int  j=0;int n=s.size();
+        for(int i=0;i<t.size() & j<n;i++){
+            if(s[j]==t[i]){
+                j++;
             }
         }
-       
-       return dp[m][n]; 
-
-    }
-     
-    
-    
-    bool isSubsequence(string s, string t) {
-        
-        int m=s.size();int n=t.size();
-       
-        
-       int LCS=lcs( m,  n, s, t);
-        
-        return LCS==m;
-        
+        return j==n;
     }
 };
