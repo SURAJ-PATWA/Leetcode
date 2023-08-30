1b3ac5bb326c660ae2534900aecc761e4213dba7
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-08-30 18:45:19 +0530

    Time: 15 ms (52.29%), Space: 20.5 MB (18.81%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -6,6 +6,7 @@ public:
         left[0]=height[0];
         vector<int>right(n);
         right[n-1]=height[n-1];
+        
         for(int i=1;i<n;i++){
             left[i]=max(left[i-1],height[i]);
         }
