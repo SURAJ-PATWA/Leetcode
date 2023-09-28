765ad05a210933b90f2e3a2b037fd59e41ba95a8
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-09-28 09:42:03 +0530

    Time: 11 ms (24.58%), Space: 16.5 MB (36.74%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -2,24 +2,36 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         int n=nums.size();
-        if(n==0)return nums;
-      vector<int>odd;
-        vector<int>even;
-        for(auto it:nums){
-            if(it%2==0){
-                even.push_back(it);
-            }
-            else{
-                odd.push_back(it);
+//         if(n==0)return nums;
+//       vector<int>odd;
+//         vector<int>even;
+//         for(auto it:nums){
+//             if(it%2==0){
+//                 even.push_back(it);
+//             }
+//             else{
+//                 odd.push_back(it);
+//             }
+//         }
+        
+//         sort(odd.begin(),odd.end());
+//         sort(even.begin(),even.end());
+        
+//         for(auto it:odd){
+//             even.push_back(it);
+//         }
+//         return even;
+        
+        // try to solve 2 pointer
+        
+        int i=0, j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0){
+                swap(nums[i],nums[j]);
+                j++;
             }
         }
+        return nums;
         
-        sort(odd.begin(),odd.end());
-        sort(even.begin(),even.end());
-        
-        for(auto it:odd){
-            even.push_back(it);
-        }
-        return even;
     }
 };
