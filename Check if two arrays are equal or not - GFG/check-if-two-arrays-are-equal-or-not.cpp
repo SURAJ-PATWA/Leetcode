93d6d5958b7aea64727575021315d61c5f5202f5
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-12-24 00:55:33 +0530

    Added solution - LeetHub

--- Check if two arrays are equal or not - GFG/check-if-two-arrays-are-equal-or-not.cpp
+++ Check if two arrays are equal or not - GFG/check-if-two-arrays-are-equal-or-not.cpp
@@ -15,11 +15,8 @@ class Solution{
     bool check(vector<ll> A, vector<ll> B, int N) {
         //code here
         sort(A.begin(),A.end());
-                sort(B.begin(),B.end());
-                
-                return A==B;
-
-        
+        sort(B.begin(),B.end());
+        return A==B;
     }
 };
 
